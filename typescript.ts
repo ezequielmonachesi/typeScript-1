8c882851dc555335361cb56a6376d4b779e41bd2
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-29 18:13:48 -0300

    Objetos en funcion con tipo de datos

--- typescript.ts
+++ typescript.ts
@@ -4,13 +4,9 @@
 //     age: 30
 // }
 
-
-
 // Cuando inicializo una variable el autocompletado de acciones es sobre ese tipo de datos.
 // let texto = 123;
 // texto.
-
-
 
 // cualquier tipo de dato ANY
 // Ignore el tipado de typescript
@@ -19,8 +15,22 @@
 // evitar any
 
 // inferencia
-// como a y b infiere que son number sin decirla nada a 
-const a = 1
-const b = 2
-const c = a + b
+// como a y b infiere que son number sin decirla nada a
+const a = 1;
+const b = 2;
+const c = a + b;
 // c también será number
+
+// const saludar = (name: string) =>{
+//     console.log(`Hola ${name}`)
+// }
+
+// saludar('Pepe')
+// saludar(2)
+
+// Objetos en funcion con tipo de datos
+// const saludar = ({ name, age }: { name: string; age: number }) => {
+//   console.log(`hola ${name}, tienes ${age} años.`);
+// };
+
+// saludar({ name: 'Pepe', age: 5 });

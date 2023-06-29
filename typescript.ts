658782fff28de1b96b99d0bd0a378ae062a631cb
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-29 18:58:51 -0300

    tipar funciones

--- typescript.ts
+++ typescript.ts
@@ -16,9 +16,9 @@
 
 // inferencia
 // como a y b infiere que son number sin decirla nada a
-const a = 1;
-const b = 2;
-const c = a + b;
+// const a = 1;
+// const b = 2;
+// const c = a + b;
 // c también será number
 
 // const saludar = (name: string) =>{
@@ -34,3 +34,36 @@ const c = a + b;
 // };
 
 // saludar({ name: 'Pepe', age: 5 });
+
+//
+// const saludar = ({ name, age }: { name: string; age: number }) => {
+//   console.log(`hola ${name}, tienes ${age} años.`)
+//   return age
+// }
+
+// let userName: string
+// userName = saludar({name: 'Pepe', age: 2})
+
+
+// cuando una funcion no tiene return se le indica un void
+// const sayHiFromFunction = (fn: (name: string) => void) => {
+//     fn('Miguel');
+// }
+
+// sayHiFromFunction((name: string)=>{
+//     console.log(`Hola ${name}`)
+// })
+
+// tipar arrow function
+const sumar = (a: number, b:number): number =>{
+    return a + b
+}
+
+const restar: (a:number, b:number) => number = (a,b) =>{
+    return a -b
+}
+
+
+
+
+// https://www.youtube.com/watch?v=fUgxxhI_bvc 43:43

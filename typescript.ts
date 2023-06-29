924dd11aabf7514813696e8724756542fed0eca1
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-06-29 17:40:17 -0300

    any, ignora el tipo de dato

--- typescript.ts
+++ typescript.ts
@@ -1,1 +1,19 @@
-const name = 'Ezequiel'
+// Cuando se crea un objeto las key reciben un tipo de dato segun le escriba.
+// const persona = {
+//     name: 'Pepe',
+//     age: 30
+// }
+
+
+
+// Cuando inicializo una variable el autocompletado de acciones es sobre ese tipo de datos.
+// let texto = 123;
+// texto.
+
+
+
+// cualquier tipo de dato ANY
+// Ignore el tipado de typescript
+// let anyValue: any = 'hola'
+// anyValue.
+
